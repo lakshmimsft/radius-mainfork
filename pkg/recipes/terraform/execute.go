5178f16d87b6efcb6b5b3ebb05c84d90ffdf0971
Lakshmi Javadekar <103459615+lakshmimsft@users.noreply.github.com>
2024-10-07 01:01:10 -0700

    Update POC
    Signed-off-by: Lakshmi Javadekar <103459615+lakshmimsft@users.noreply.github.com>

--- pkg/recipes/terraform/execute.go
+++ pkg/recipes/terraform/execute.go
@@ -33,11 +33,10 @@ import (
     "k8s.io/client-go/tools/clientcmd"
 )
 
-// Deploy triggers a Kubernetes Job to run the Terraform commands for deploying the infrastructure.
 func Deploy(ctx context.Context, workingDir, recipeName string) error {
     // Check if the working directory exists.
     if _, err := os.Stat(workingDir); os.IsNotExist(err) {
-        return errors.New("working directory does not exist")
+        return fmt.Errorf("working directory does not exist")
     }
 
     // Initialize the Kubernetes client.
@@ -52,17 +51,181 @@ func Deploy(ctx context.Context, workingDir, recipeName string) error {
         return fmt.Errorf("failed to create ConfigMap: %v", err)
     }
 
-    // Create a Kubernetes Job to run the Terraform commands.
-    jobName := fmt.Sprintf("terraform-job-%s", recipeName)
-    if err := createTerraformJob(ctx, clientset, jobName, configMapName); err != nil {
+    // Create a Kubernetes Job to run the Terraform apply command.
+    jobName := fmt.Sprintf("terraform-apply-job-%s", recipeName)
+    if err := createTerraformApplyJob(ctx, clientset, jobName, configMapName); err != nil {
         return fmt.Errorf("failed to create Kubernetes Job: %v", err)
     }
 
-    // Optionally, wait for the Job to complete.
-    // err = waitForJobCompletion(ctx, clientset, jobName)
+    // Wait for the Job to complete.
+    err = WaitForJobCompletion(ctx, clientset, jobName, "default")
+    if err != nil {
+        return fmt.Errorf("deployment failed: %v", err)
+    }
+
+    // clean up resources - keep for debugging
+    // err = cleanupJobAndConfigMap(ctx, clientset, jobName, configMapName, "default")
     // if err != nil {
-    //     return fmt.Errorf("job failed: %v", err)
+    //     return fmt.Errorf("cleanup failed: %v", err)
     // }
+
+    return nil
+}
+
+// Delete triggers a Kubernetes Job to run the Terraform commands for destroying the infrastructure.
+func Delete(ctx context.Context, workingDir, recipeName string) error {
+    // Check if the working directory exists.
+    if _, err := os.Stat(workingDir); os.IsNotExist(err) {
+        return fmt.Errorf("working directory does not exist")
+    }
+
+    // Initialize the Kubernetes client.
+    clientset, err := getKubernetesClient()
+    if err != nil {
+        return fmt.Errorf("failed to initialize Kubernetes client: %v", err)
+    }
+
+    // Create a ConfigMap with the Terraform configuration.
+    configMapName := fmt.Sprintf("terraform-config-%s", recipeName)
+    if err := createConfigMap(ctx, clientset, configMapName, workingDir); err != nil {
+        return fmt.Errorf("failed to create ConfigMap: %v", err)
+    }
+
+    // Create a Kubernetes Job to run the Terraform destroy command.
+    jobName := fmt.Sprintf("terraform-destroy-job-%s", recipeName)
+    if err := createTerraformDestroyJob(ctx, clientset, jobName, configMapName); err != nil {
+        return fmt.Errorf("failed to create Kubernetes Job: %v", err)
+    }
+
+    // Wait for the Job to complete.
+    err = WaitForJobCompletion(ctx, clientset, jobName, "default")
+    if err != nil {
+        return fmt.Errorf("deletion failed: %v", err)
+    }
+
+    // Clean up resources.
+    err = cleanupJobAndConfigMap(ctx, clientset, jobName, configMapName, "default")
+    if err != nil {
+        return fmt.Errorf("cleanup failed: %v", err)
+    }
+
+    return nil
+}
+
+// createTerraformDestroyJob creates a Kubernetes Job that runs 'terraform destroy'.
+func createTerraformDestroyJob(ctx context.Context, clientset *kubernetes.Clientset, jobName, configMapName string) error {
+    job := &batchv1.Job{
+        ObjectMeta: metav1.ObjectMeta{
+            Name: jobName,
+        },
+        Spec: batchv1.JobSpec{
+            Template: corev1.PodTemplateSpec{
+                ObjectMeta: metav1.ObjectMeta{
+                    Labels: map[string]string{
+                        "job-name": jobName,
+                    },
+                },
+                Spec: corev1.PodSpec{
+                    RestartPolicy: corev1.RestartPolicyNever,
+                    Containers: []corev1.Container{
+                        {
+                            Name:  "terraform",
+                            Image: "hashicorp/terraform:light",
+                            Command: []string{
+                                "sh",
+                                "-c",
+                                "terraform init && terraform destroy -auto-approve",
+                            },
+                            WorkingDir: "/app/terraform",
+                            VolumeMounts: []corev1.VolumeMount{
+                                {
+                                    Name:      "terraform-config",
+                                    MountPath: "/app/terraform",
+                                },
+                            },
+                        },
+                    },
+                    Volumes: []corev1.Volume{
+                        {
+                            Name: "terraform-config",
+                            VolumeSource: corev1.VolumeSource{
+                                ConfigMap: &corev1.ConfigMapVolumeSource{
+                                    LocalObjectReference: corev1.LocalObjectReference{
+                                        Name: configMapName,
+                                    },
+                                },
+                            },
+                        },
+                    },
+                },
+            },
+        },
+    }
+
+    // Create the Job in the 'default' namespace - temporary.
+    _, err := clientset.BatchV1().Jobs("default").Create(ctx, job, metav1.CreateOptions{})
+    if err != nil {
+        return fmt.Errorf("failed to create Kubernetes Job: %v", err)
+    }
+
+    return nil
+}
+
+// createTerraformApplyJob creates a Kubernetes Job that runs 'terraform apply'.
+func createTerraformApplyJob(ctx context.Context, clientset *kubernetes.Clientset, jobName, configMapName string) error {
+    job := &batchv1.Job{
+        ObjectMeta: metav1.ObjectMeta{
+            Name: jobName,
+        },
+        Spec: batchv1.JobSpec{
+            Template: corev1.PodTemplateSpec{
+                ObjectMeta: metav1.ObjectMeta{
+                    Labels: map[string]string{
+                        "job-name": jobName,
+                    },
+                },
+                Spec: corev1.PodSpec{
+                    RestartPolicy: corev1.RestartPolicyNever,
+                    Containers: []corev1.Container{
+                        {
+                            Name:  "terraform",
+                            Image: "hashicorp/terraform:light",
+                            Command: []string{
+                                "sh",
+                                "-c",
+                                "terraform init && terraform apply -auto-approve",
+                            },
+                            WorkingDir: "/app/terraform",
+                            VolumeMounts: []corev1.VolumeMount{
+                                {
+                                    Name:      "terraform-config",
+                                    MountPath: "/app/terraform",
+                                },
+                            },
+                        },
+                    },
+                    Volumes: []corev1.Volume{
+                        {
+                            Name: "terraform-config",
+                            VolumeSource: corev1.VolumeSource{
+                                ConfigMap: &corev1.ConfigMapVolumeSource{
+                                    LocalObjectReference: corev1.LocalObjectReference{
+                                        Name: configMapName,
+                                    },
+                                },
+                            },
+                        },
+                    },
+                },
+            },
+        },
+    }
+
+    // Create the Job in the 'default' namespace - temporary.
+    _, err := clientset.BatchV1().Jobs("default").Create(ctx, job, metav1.CreateOptions{})
+    if err != nil {
+        return fmt.Errorf("failed to create Kubernetes Job: %v", err)
+    }
 
     return nil
 }
@@ -196,7 +359,7 @@ func createTerraformJob(ctx context.Context, clientset *kubernetes.Clientset, jo
     return nil
 }
 
-// WaitForJobCompletion waits for the Kubernetes Job to complete.
+// WaitForJobCompletion waits for the Kubernetes Job to complete. - Add Logger
 func WaitForJobCompletion(ctx context.Context, clientset *kubernetes.Clientset, jobName string, namespace string) error {
     watchInterface, err := clientset.BatchV1().Jobs(namespace).Watch(ctx, metav1.ListOptions{
         FieldSelector: fmt.Sprintf("metadata.name=%s", jobName),
